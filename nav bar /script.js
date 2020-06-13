18964b01c9e7dd671a0bb7d29ccf0f3054166ef8
varaprasadsgowda <varaprasadsaaa@gmail.com>
2020-06-13 08:39:57 +0530

    cccc

--- nav bar /script.js
+++
@@ -1,8 +0,0 @@
-function openslidemenu(){
-    document.getElementById('side-menu').style.width = '250px'
-    document.getElementById('main').style.margin = '250px';
-}
-function closeslidemenu(){
-    document.getElementById('side-menu').style.width = '0px'
-    document.getElementById('main').style.margin = '0px';
-}
